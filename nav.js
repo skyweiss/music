3b43511fa4d64ae5373e547cfbda75d17549b52e
skyweiss <54119070+skyweiss@users.noreply.github.com>
2025-02-21 09:14:30 -0600

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,14 @@
+document.getElementById("navigation").innerHTML =
+"<h2>Sky Weiss</h2>" +
+"<table>" +
+"<tr>" +
+"<td><b>2020</b></td>" +
+"<td><a href='mitm.html'>Made It to Midnight</a>" +
+"</tr>" +
+"<tr>" +
+"<td><b>2019</b></td>" +
+"<td><a href='fallforyou.html'>Fall for You</a></td>" +
+"</tr>" +
+"</table>";
+
+document.getElementById("copyright").innerHTML = "&copy; " + new Date().getFullYear();
